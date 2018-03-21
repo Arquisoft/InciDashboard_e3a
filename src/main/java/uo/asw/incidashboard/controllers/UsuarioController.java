119292ac3eae9007db077dd38dfc13dd7299d298
espiguinho <alvarocabrerobarros@gmail.com>
2018-03-21 16:17:59 +0100

    Arreglos

--- src/main/java/uo/asw/incidashboard/controllers/UsuarioController.java
+++ src/main/java/uo/asw/incidashboard/controllers/UsuarioController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class Controllers {
+public class UsuarioController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
--- src/main/java/uo/asw/incidashboard/repositories/UsuarioRepository.java
+++ src/main/java/uo/asw/incidashboard/repositories/UsuarioRepository.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import uo.asw.dbManagement.model.Usuario;
-import uo.asw.incidashboard.models.Ejemplo;
+
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
