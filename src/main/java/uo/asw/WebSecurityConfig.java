c16dfc64406a70bc247b01b734c66769c45b4dee
espiguinho <alvarocabrerobarros@gmail.com>
2018-03-24 20:06:12 +0100

    Falta el poder identificar a cada usuario a su html correspondiente

--- src/main/java/uo/asw/WebSecurityConfig.java
+++ src/main/java/uo/asw/WebSecurityConfig.java
@@ -1,10 +1,11 @@
 package uo.asw;
 
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -32,6 +33,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
-		
+
+	}
+
+	@Bean
+	public AuthenticationManager customAuthenticationManager() throws Exception {
+		return authenticationManager();
 	}
 }
--- src/main/java/uo/asw/incidashboard/controllers/HomeController.java
+++ src/main/java/uo/asw/incidashboard/controllers/HomeController.java
@@ -1,19 +1,47 @@
 package uo.asw.incidashboard.controllers;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import uo.asw.dbManagement.model.Usuario;
+import uo.asw.dbManagement.tipos.PerfilTipos;
+import uo.asw.incidashboard.services.SecurityService;
+import uo.asw.incidashboard.services.UsuarioService;
+
 @Controller
 public class HomeController {
+	@Autowired
+	private UsuarioService usuarioService;
+	@Autowired
+	private SecurityService secService;
 
 	@RequestMapping("/")
 	public String index() {
 		return "index";
 	}
+
+	// @RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	// public String home(Model model) {
+	// return "home";
+	// }
+
 	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
-	public String home(Model model) {
-		return "home";
+	public String setEdit(Model model) {
+		String mail = secService.findLoggedInEmail();
+		Usuario user = usuarioService.getUsuarioByIdentificador(mail);
+		if (user.getPerfil().equals(PerfilTipos.OPERARIO))
+			return "redirect:/operarios/operario";
+		else if (user.getPerfil().equals(PerfilTipos.ADMIN_CM))
+			return "redirect:/operarios/administrador";
+		else if (user.getPerfil().equals(PerfilTipos.ANALISIS_DATOS))
+			return "redirect:/operarios/responsableanalisis";
+		return "redirect:/login";
+
 	}
+
 }
