4f42f10203b37b5118537ac0f0f2fd1dabe9c539
espiguinho <alvarocabrerobarros@gmail.com>
2018-03-25 16:49:56 +0200

    Errores arreglados

--- src/main/java/uo/asw/InciDashboardApplication.java
+++ src/main/java/uo/asw/InciDashboardApplication.java
@@ -2,8 +2,14 @@ package uo.asw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan({"com.delivery.request"})
+@EntityScan("com.delivery.domain")
+
 public class InciDashboardApplication {
 	// Ahora no te dejará lanzarla porque no está mapeada la clase que se indica en el EjemploRepository
 	public static void main(String[] args) {
