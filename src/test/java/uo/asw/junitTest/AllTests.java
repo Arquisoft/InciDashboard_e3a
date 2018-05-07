8a4596a839bd8745965c729096b4eee89ea1ca31
Pelayo Díaz Soto <UO251000@uniovi.es>
2018-05-07 19:47:01 +0200

    Añadidos JUnit Básicos

---
+++ src/test/java/uo/asw/junitTest/AllTests.java
@@ -0,0 +1,16 @@
+package uo.asw.junitTest;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	
+	CategoriaTest.class,
+	IncidenciaBDTest.class,
+	PropiedadTest.class,
+	UsuarioBDTest.class,
+	ValorLimiteTest.class
+})
+public class AllTests { }
